2bf99fc789f43fc60fe761a2545269673abd1a1a
kastur <kastur@gmail.com>
2012-08-03 01:37:27 -0700

    Initial commit.

--- OverrideService/gen/android/override/R.java
+++ OverrideService/gen/android/override/R.java
@@ -10,10 +10,12 @@ package android.override;
 public final class R {
     public static final class attr {
     }
+
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int icon = 0x7f020000;
     }
+
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name = 0x7f030000;
     }
 }

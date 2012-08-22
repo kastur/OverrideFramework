ac601ff7a1449d58354d63bd614a5ccb5a81bc74
kastur <kastur@gmail.com>
2012-08-21 21:06:37 -0700

    OverrideCommander restores last command state of OLM.

--- OverrideService/src/android/override/OverrideCommander.java
+++ OverrideService/src/android/override/OverrideCommander.java
@@ -12,9 +12,9 @@ public class OverrideCommander extends IOverrideCommander.Stub {
 
   private static final String TAG = "OverrideCommander";
 
-  private HashSet<IOverrideCommandListener> mListeners = new HashSet<IOverrideCommandListener>();
-  private HashMap<String, HashSet<IOverrideCommandListener>> mListenersByPackage =
+  private HashMap<String, HashSet<IOverrideCommandListener>> mListenerSets =
       new HashMap<String, HashSet<IOverrideCommandListener>>();
+  private HashMap<String, Bundle> mLastCommand = new HashMap<String, Bundle>();
 
   @Override
   public void onStatusChanged(String packageName, Bundle data) throws RemoteException {
@@ -25,22 +25,33 @@ public class OverrideCommander extends IOverrideCommander.Stub {
   public synchronized void registerCommandListener(String packageName,
                                                    IOverrideCommandListener listener)
       throws RemoteException {
-    mListeners.add(listener);
-    if (!mListenersByPackage.containsKey(packageName)) {
-      mListenersByPackage.put(packageName, new HashSet<IOverrideCommandListener>());
+    if (!mListenerSets.containsKey(packageName)) {
+      mListenerSets.put(packageName, new HashSet<IOverrideCommandListener>());
     }
-    mListenersByPackage.get(packageName).add(listener);
+    mListenerSets.get(packageName).add(listener);
+
+    Bundle lastCommand = mLastCommand.get(packageName);
+
+    if (lastCommand != null) {
+      // TODO: This sends out the last command to all listeners, not just the last registered one.
+      relayCommandToListeners(packageName, lastCommand);
+    }
   }
 
   @Override
   public void removeCommandListener(String packageName, IOverrideCommandListener listener)
       throws RemoteException {
-    mListeners.remove(listener);
-    mListenersByPackage.get(packageName).remove(listener);
+    mListenerSets.get(packageName).remove(listener);
   }
 
-  public void onCommand(String packageName, Bundle data) {
-    HashSet<IOverrideCommandListener> listeners = mListenersByPackage.get(packageName);
+  private void handleCommandReceived(String packageName, Bundle command) {
+    final Bundle savedCommand = new Bundle(command);
+    mLastCommand.put(packageName, savedCommand);
+    relayCommandToListeners(packageName, command);
+  }
+
+  private void relayCommandToListeners(String packageName, Bundle command) {
+    HashSet<IOverrideCommandListener> listeners = mListenerSets.get(packageName);
     if (listeners == null) {
       Log.v(TAG, "No listeners registered for " + packageName);
       return;
@@ -48,14 +59,16 @@ public class OverrideCommander extends IOverrideCommander.Stub {
 
     for (IOverrideCommandListener listener : listeners) {
       try {
-        listener.onCommand(data);
+        Log.d(TAG, "sendCommand: " + packageName);
+        Log.d(TAG, "sendCommand: " + command.toString());
+        listener.onCommand(command);
       } catch (RemoteException ex) {
-        Log.d(TAG, "onCommand: " + packageName + " RemoteException!");
+        Log.d(TAG, "sendCommand: " + packageName + " RemoteException!");
       }
     }
   }
 
   public void onReceiveData(Bundle data) {
-    onCommand(data.getString("PACKAGE"), data);
+    handleCommandReceived(data.getString("PACKAGE"), data);
   }
 }
